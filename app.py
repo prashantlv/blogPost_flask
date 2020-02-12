c32f2d0d3652fe18b78233cf3ea7eb742eb840f2
Prashant Shukla <shukla.prashant689@gmail.com>
2020-02-12 10:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,20 @@ def posts():
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html',posts = all_posts)
 
+@app.route('/posts/new', methods = ['GET','POST'])        
+def new_post():
+    if request.method == 'POST':
+        # read from form and add them to db
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title = post_title, content= post_content, author = post_author)
+        db.session.add(new_post) # add data in db for current session
+        db.session.commit()     # save permanently in file
+
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')    
 @app.route('/posts/delete/<int:id>')
 def delete(id):
     post = BlogPost.query.get_or_404(id)
@@ -76,19 +90,7 @@ def edit(id):
         return redirect('/posts')
     else:
         return render_template('edit.html', post = post)    
-@app.route('/users/<string:name>/<int:id>')  # thr < > we can retrive data from dynamic url 
-def hello(name,id):
-    return "Hello, " + name + " your id is: " + str(id) + "."
 
-@app.route('/onlyget', methods = ['GET','POST'])  
-def get_req():
-    return "<h1>You can only get this page.</h1>"   #we can pass html tags in it
-@app.route('/test')
-def test():
-    return '''
-    <h2> h2 </h2>
-    <h4> h4 </h4>
-    '''       
 
 if __name__ == "__main__":
     app.run(debug=True)
